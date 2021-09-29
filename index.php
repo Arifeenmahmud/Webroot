6162ea049af3c3c6bff58fde738cf866dedbeff6
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2021-09-29 19:32:01 +0600

    Format Indentation

--- index.php
+++ index.php
@@ -9,17 +9,17 @@ $dbname = "pgd-it";
 // $conn = new mysqli($servername, $username, $password, $dbname);
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-$data=mysqli_query($conn,"SELECT `ID No.`,`Name`,`Phone`,`Department`,`Mail Address`,`Address` FROM `ju-13`");
+$data = mysqli_query($conn, "SELECT `ID No.`,`Name`,`Phone`,`Department`,`Mail Address`,`Address` FROM `ju-13`");
 echo "<center><table border=1>";
-while($row=mysqli_fetch_assoc($data)){
-echo "<tr>";
-    echo "<td>".$row['ID No.']."</td>".
-         "<td>".$row['Name']."</td>".
-         "<td>".$row['Phone']."</td>".
-         "<td>".$row['Department']."</td>".
-         "<td>".$row['Mail Address']."</td>".
-         "<td>".$row['Address']."</td>";
-echo "</tr>";
+while ($row = mysqli_fetch_assoc($data)) {
+    echo "<tr>";
+    echo "<td>" . $row['ID No.'] . "</td>" .
+        "<td>" . $row['Name'] . "</td>" .
+        "<td>" . $row['Phone'] . "</td>" .
+        "<td>" . $row['Department'] . "</td>" .
+        "<td>" . $row['Mail Address'] . "</td>" .
+        "<td>" . $row['Address'] . "</td>";
+    echo "</tr>";
 }
 echo "</table></center>";
 ?>
